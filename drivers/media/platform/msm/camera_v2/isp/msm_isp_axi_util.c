82170e93f3a57a0e36d8e4eb6430fc5bf2865337
dianlujitao <dianlujitao@lineageos.org>
2023-01-15 12:05:04 +0530

    msm: camera_v2: conditionality eliminate drop_reconfig flag
    The flag is set to notify userspace driver, and expected to be clear
    there. However, our userspace driver isn't able to handle this, as a
    result the camera stopped working once it's set because an empty frame
    is always returned.
    
    This conditionality reverts bc82952
    and 39e7df3
    
    Change-Id: I598d8ffc5f2d8eb1fe387a5210e0ef97d2e3badd
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -3641,7 +3641,8 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		) {
 		pr_debug("%s:%d invalid time to request frame %d try drop_reconfig\n",
 			__func__, __LINE__, frame_id);
-#ifdef CONFIG_MACH_XIAOMI_JASON
+
+#ifdef CONFIG_MACH_XIAOMI_LAVENDER
 		goto error;
 #else
 		vfe_dev->isp_page->drop_reconfig = 1;
@@ -3690,7 +3691,8 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, __LINE__, vfe_dev->pdev->id, frame_id,
 			stream_info->activated_framedrop_period,
 			stream_info->stream_id);
-#ifdef CONFIG_MACH_XIAOMI_JASON
+
+#ifdef CONFIG_MACH_XIAOMI_LAVENDER
 		rc = msm_isp_return_empty_buffer(vfe_dev, stream_info,
 			user_stream_id, frame_id, buf_index, frame_src);
 		if (rc < 0)
